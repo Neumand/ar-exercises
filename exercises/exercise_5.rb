3ba2cec445bc86c51b1fddbf0f817166423972d7
David Neuman <david.m.neuman@gmail.com>
2019-04-16 16:52:44 +0000

    Completed exercise 5.

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+total_revenue = Store.sum("annual_revenue")
+puts "Total company revenue is #{total_revenue}"
+
+avg_annual_revenue = Store.average("annual_revenue")
+puts "Average annual revenue is #{avg_annual_revenue}"
+
+nb_stores_over_1M = Store.where("annual_revenue > 1000000").count
+puts "There are #{nb_stores_over_1M} stores with over 1M in annual revenue"
